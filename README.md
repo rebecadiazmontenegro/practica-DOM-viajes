58877b4edf73911ec4e56c3af9235e7dd3752163
Rebeca D.Montenegro <rebediazmontenegro@gmail.com>
2025-10-06 16:08:07 +0200

    Add Carpetas

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# practica-DOM-viajes
+
+---
+## Utilizando las fotos de ejemplo, generar una web como la que se puede ver en el boceto pasado. Utilizar HTML,CSS, JS.
+
+#NOTA: Las imágenes de los viajes y opciones del selector deben ser generadas haciendo manipulación del DOM con JS. No hace falta generar absolutamente todo desde JS. Lo que sea estático como header, footer, foto principal, etc... se añade directamente en el HTML, y cosas dinámicas como las tarjetas de destinos preferidos  y ciudades, añadir con JS
+
+Para los desitinos recomendados, podéis crearos un array de objetos que contenga los datos y las URL de las imágenes que os tenéis que descargar. Después, podéis iterar con un bucle y renderizar en el DOM:
---
+++ js/favoritos.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    title: "Sevilla",
+    description: "Sevilla es la capital de Andalucía, famosa por su historia, su arte, la Giralda, el Alcázar y su ambiente alegre y tradicional.",
+    url_img: "./assets/viajes/viajes-4.jpg"
+  },
+  {
+    title: "Madrid",
+    description: "Madrid es la capital de España, una ciudad moderna y vibrante, conocida por su cultura, su vida nocturna y museos como el Prado y el Reina Sofía.",
+    url_img: "./assets/viajes/viajes-2.jpg"
+  },
+  {
+    title: "Albacete",
+    description: "Albacete es una ciudad de Castilla-La Mancha, conocida por su industria cuchillera, su feria y su ambiente tranquilo y acogedor.",
+    url_img: "./assets/viajes/viajes-1.jpg"
+  }
+];
+
+export default pokemons
